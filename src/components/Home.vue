e11b73b1ff631639682d6e7ebe0d25f9380ba62e
unknown <shimijoseph94@gmail.com>
2019-06-29 14:21:20 +0530

    first

---
+++ src/components/Home.vue
@@ -0,0 +1,276 @@
+<template>
+  <div>
+    <div class="row">
+   <div class="col-md-12 row">
+     <div class="col-md-6" style="padding-top:140px!important;padding-left:140px!important;">
+     <h4 style="color:black!important;text-align: justify!important;"><strong>Hi I am </strong></h4>
+     <h4 style="color:black!important;text-align: justify!important;"><strong>Romulus Whitaker</strong> </h4>
+     <br>
+     <h6 style="color:black!important;text-align: justify!important;"> Indian herpetologist, Wildlife conservationist </h6>
+    <br>
+    <!--<div style="text-align: left!important;">
+    <button class="button">GET IN TOUCH</button>
+    </div>-->
+    <div style="text-align: left!important;">
+    <a href="#" class="square_btn">GET IN TOUCH</a>
+    </div>
+    
+    <br>
+    <br>
+    <h4 style="color:black!important;text-align: justify!important;padding-top:40px!important;">FOLLOW ME</h4>
+    <p style="text-align: left;">
+    <a href="#">
+      <font-awesome-icon :icon="['fab', 'facebook-f']" style=" border-radius: 50%;  border: 1x solid grey;
+      padding:10px;  width:50px; height:50px;" size="4x" /></a>
+      
+      <a href="#" style="padding-left:10px!important;">
+        <font-awesome-icon :icon="['fab', 'instagram']" style=" border-radius: 50%;  border: 1x solid grey;
+      padding:10px; width:50px; height:50px;" size="4x" /></a>
+    
+      <a href="#" style="padding-left:10px!important;">
+        <font-awesome-icon :icon="['fab', 'twitter']" style=" border-radius: 50%;  border: 1x solid grey;
+   padding:10px;  width:50px; height:50px;" size="4x" /></a>
+    </p>
+    </div>
+     <div class="col-md-6" style="margin-top: 0px;">
+      <!--<div class="header">
+            <h1></h1>
+          </div>-->
+      <!--<p id="rcorners1" style="margin-top:0;"></p>-->
+      <div class="figure">
+        <img src="../assets/rom.jpeg" style="padding-top:30px;height:650px;">
+      </div>
+     </div>
+   </div>
+   </div> 
+   <!--2nd section-->
+   <div class="container">
+   <div class="row" style="padding-top:90px!important;">
+    
+    
+     <div class="col-md-12 row" >
+       <div class="col-md-6">
+         
+       <img src="../assets/rome.jpeg" style="width:100%; height: auto;">
+          
+       </div>
+       <div class="col-md-6">
+          <h3 style="color:black!important;text-align: justify!important;"><strong>ABOUT</strong></h3>
+          <p class="para" style="text-align:justify!important;">Romulus Earl Whitaker (born 23 May 1943) is an Indian herpetologist, wildlife conservationist and founder of the Madras Snake Park, The Andaman and Nicobar Environment Trust (ANET), and the Madras Crocodile Bank Trust. In 2008, Whitaker was selected as an Associate Laureate in the 2008 Rolex Awards for Enterprise, for his efforts to create a network of rainforest research stations throughout India.In 2005 he was a winner of a Whitley Award for outstanding leadership in nature conservation. He used this award and found the Agumbe Rainforest Research Station in Karnataka, for the study of King Cobras and their habitat.</p>
+          <a href="#" class="square_btn">KNOW MORE</a>
+         </div>
+     </div>
+     </div>
+   </div>
+
+<!-- 3rd section-->
+<!--<div class="container">
+ <div class="row">
+   <div class="col-md-12 row">
+      <h4 style="color:black!important;text-align: justify!important;">WORKS</h4>
+     <div class="col-md-6">
+      <h4 style="color:black!important;text-align: justify!important;">Snake Park Chennai</h4>
+      <p class="para">Whitaker was the founder director of the Snake Park in Chennai. The park was conceived to rehabilitate the Irula tribe who are known for their expertise in catching snakes. The tribals were left jobless after the ban of snake trading. Whitaker helped the Irula tribe to get involved in extracting snake venom used for the production of antivenom drugs. Rom is the Founder-Director of the Madras Crocodile Bank Trust Centre for Herpetology, actively involved in crocodile breeding and conservation programs.</p>
+     </div>
+     <div class="col-md-6">
+        <h4 style="color:black!important;text-align: justify!important;">Snake Park Chennai</h4>
+        <p class="para">Whitaker was the founder director of the Snake Park in Chennai. The park was conceived to rehabilitate the Irula tribe who are known for their expertise in catching snakes. The tribals were left jobless after the ban of snake trading. Whitaker helped the Irula tribe to get involved in extracting snake venom used for the production of antivenom drugs. Rom is the Founder-Director of the Madras Crocodile Bank Trust Centre for Herpetology, actively involved in crocodile breeding and conservation programs.</p>
+       </div>
+     
+   </div>
+ </div> 
+</div>-->
+
+<div class="container">
+    <h3 style="color:black!important;text-align:center!important;padding-top:90px!important;"><strong>WORKS</strong></h3>
+    <div class="row" style="padding-top:40px!important;">
+       
+
+      <div class="col-md-12 row" >
+          <div class="col-md-6">
+              <h4 style="color:black!important;text-align: justify!important;">MADRAS CROCODILE BANK TRUST</h4>
+              <p class="para">The Madras Crocodile Bank Trust and Centre for Herpetology (or Croc Bank) was the brain child of the legendary Romulus Whitaker and a handful of like-minded conservation visionaries who began work on the facility in 1976, in a desperate effort to save India’s dwindling crocodilian populations. Today, after more than 30 formidable years of cutting edge science and grassroots education, the Croc Bank remains a world leader in the field of frontline conservation and the preservation of natural landscapes.
+   
+               </p>
+              <!--<button class="button">KNOW MORE</button>-->
+              
+               <a href="#" class="square_btn">KNOW MORE</a>
+              
+              <br>
+              <br>
+             </div>
+        <div class="col-md-6">
+            <h4 style="color:black!important;text-align:left!important;">SNAKE PARK CHENNAI</h4>
+            <p class="para">Whitaker was the founder director of the Snake Park in Chennai. The park was conceived to rehabilitate the Irula tribe who are known for their expertise in catching snakes. The tribals were left jobless after the ban of snake trading. Whitaker helped the Irula tribe to get involved in extracting snake venom used for the production of antivenom drugs. Rom is the Founder-Director of the Madras Crocodile Bank Trust Centre for Herpetology, actively involved in crocodile breeding and conservation programs.</p>
+      
+              <a href="#" class="square_btn">KNOW MORE</a>
+              
+        </div>
+      
+      </div>
+      </div>
+    </div>
+
+<!--portfolio-->
+<h3 style="color:black!important;text-align: center!important;padding-top:60px;padding-bottom:50px!important;"><strong>PORTFOLIO</strong></h3>
+<div class="container">
+    <div class="row"> 
+        <div class="col-md-3">
+      <img src="../assets/r7.jpeg" style="width:250px; height: 250px;">
+        </div>
+        <div class="col-md-3">
+      <img src="../assets/r8.jpeg" style="width:250px; height: 250px;">
+        </div>
+        <div class="col-md-3">
+      <img src="../assets/r9.jpeg" style="width:250px; height: 250px;">
+        </div>
+        <div class="col-md-3">
+      <img src="../assets/r16.jpeg" style="width:250px; height: 250px;">
+        </div>
+      </div> </div> 
+
+<div class="container">
+<div class="row" style="padding-top:40px;">
+  
+  <div class="col-md-3">
+<img src="../assets/rom.jpeg" style="width:250px; height: 250px;">
+  </div>
+  <div class="col-md-3">
+<img src="../assets/rom1.jpeg" style="width:250px; height: 250px;">
+  </div>
+  <div class="col-md-3">
+<img src="../assets/rr.jpeg" style="width:250px; height: 250px;">
+  </div>
+  <div class="col-md-3">
+<img src="../assets/r11.jpeg" style="width:250px; height: 250px;">
+  </div>
+  </div>
+</div>    
+ <br>
+ <br>
+ 
+  <div class="container">
+      <div class="row" style="padding-bottom:50px!important;"> 
+          <div class="col-md-3">
+        <img src="../assets/r12.jpeg" style="width:250px; height: 250px;">
+          </div>
+          <div class="col-md-3">
+        <img src="../assets/r13.jpeg" style="width:250px; height: 250px;">
+          </div>
+          <div class="col-md-3">
+        <img src="../assets/r14.jpeg" style="width:250px; height: 250px;">
+          </div>
+          <div class="col-md-3">
+        <img src="../assets/r15.jpeg" style="width:250px; height: 250px;">
+          </div>
+        </div> </div>   
+   
+ 
+             
+  </div>
+</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+   
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ .button{
+    background: -webkit-linear-gradient(#48c6ef , #6f86d6);
+  }
+  .fa-facebook {
+  background: #3B5998;
+  color: white;
+}
+
+.fa-facebook-f {
+    background: #3B5998;
+    color: white;
+  }
+
+.fa {
+  padding: 20px;
+  font-size: 30px;
+  width: 30px;
+  text-align: center;
+  text-decoration: none;
+  margin: 5px 2px;
+  border-radius: 50%;
+}
+
+.fa:hover {
+    opacity: 0.7;
+}
+.para {
+  color: black !important;
+    font-size: 15px !important ;
+    line-height: 1.5 !important;
+    text-align: justify !important;
+    display: block !important;
+    margin-block-start: 1em !important;
+    margin-block-end: 1em !important;
+    margin-inline-start: 0px !important;
+    margin-inline-end: 0px !important;
+}
+
+.fa-instagram {
+    color: white;
+    background: #d6249f;
+    background: radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 45%, #d6249f 60%, #285AEB 90%);
+    box-shadow: 0px 3px 10px rgba(0, 0, 0, .25);
+  }
+
+.fa-twitter {
+  background: #55ACEE;
+  color: white;
+}
+#rcorners1 {
+  border-radius: 60px;
+  border-bottom-right-radius: 60px;
+  background: cornflowerblue;
+  padding: 50px; 
+  width: 800px;
+  height: 600px; 
+  margin-top:0; 
+  background-position: bottom center;
+  background-repeat: no-repeat;
+  background-image: url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTW-N4BSVOvO0eq379RMVD65ebi7_k3nkSyJddvjvmT7BWqFCri");
+}
+.header {
+  background-image: url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTW-N4BSVOvO0eq379RMVD65ebi7_k3nkSyJddvjvmT7BWqFCri");
+  height: 500px;
+  background-position: center center;
+  background-size: cover;
+  border-radius: 50px;
+  background-repeat: no-repeat;
+  margin: 20px;
+}
+.figure {
+height: 600px;
+width: 700px;
+background-color:cornflowerblue;
+border-bottom-left-radius: 100%150px;
+border-bottom-right-radius: 100%150px; 
+background-position: center; 
+background-repeat: no-repeat;
+
+}
+.square_btn{
+    display: inline-block;
+    padding: 7px 20px;
+	  border-radius: 25px;
+    text-decoration: none;
+    color: #FFF;
+    background: cornflowerblue;
+    transition: .4s;
+}
+
+</style>
